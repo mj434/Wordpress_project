8a22062cb29e6741b12a7097363ffc3e1f3b6f13
MohitJoshi <70653516+mj434@users.noreply.github.com>
2021-09-30 15:39:12 -0400

    Wordpress project

---
+++ wordpress_data/wp-content/themes/mytheme/comments.php
@@ -0,0 +1,10 @@
+<div id="comments" class="comments-area default-max-width show-avatars">
+
+
+    <div id="respond" class="comment-respond">
+        <h2 id="reply-title" class="comment-reply-title">Leave a comment <small><a rel="nofollow" id="cancel-comment-reply-link" href="/?page_id=7&amp;preview_id=7&amp;preview_nonce=992e6c66da&amp;preview=true#respond" style="display:none;">Cancel reply</a></small></h2><form action="http://localhost:8000/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate><p class="comment-form-comment"><label for="comment">Comment</label> <textarea id="comment" name="comment" cols="45" rows="5" maxlength="65525" required="required"></textarea></p>  <a href='http://localhost:8000/?page_id=99999&amp;srp=7&amp;srk=aa1483e4e14c8a23c05ddae7e2e27fad'></a> <p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="Post Comment" /> <input type='hidden' name='comment_post_ID' value='7' id='comment_post_ID' />
+                <input type='hidden' name='comment_parent' id='comment_parent' value='0' />
+            </p><input type="hidden" id="_wp_unfiltered_html_comment_disabled" name="_wp_unfiltered_html_comment_disabled" value="2587ce2360" /><script>(function(){if(window===window.parent){document.getElementById('_wp_unfiltered_html_comment_disabled').name='_wp_unfiltered_html_comment';}})();</script>
+        </form>    </div><!-- #respond -->
+
+</div>
---
+++ wordpress_data/wp-content/themes/mytheme/footer.php
@@ -0,0 +1,6 @@
+<div id= "ttr_footer">
+    <h4><a href="http://localhost:8000/?page_id=3">Privacy Policy(Draft)</a></h4>
+</div>
+</div>
+</body>
+</html>
